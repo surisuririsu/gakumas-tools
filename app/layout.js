45d868ad7effe93bfb700e266060a33187130151
risりす <surisuririsu@gmail.com>
2024-07-25 17:48:32 -0400

    Fix body style

--- app/layout.js
+++ app/layout.js
@@ -12,9 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className} style={{ overflow: "auto" }}>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
