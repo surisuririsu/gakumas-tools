b63f860c377de65257ed2ee183560e98dd770d6f
risりす <surisuririsu@gmail.com>
2024-07-27 22:53:09 -0400

    Add GA

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import { SessionProvider } from "next-auth/react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import Footer from "@/components/Footer";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 import Main from "@/components/Main";
 import Navbar from "@/components/Navbar";
 import { DataContextProvider } from "@/contexts/DataContext";
@@ -13,22 +14,25 @@ import { WorkspaceContextProvider } from "@/contexts/WorkspaceContext";
 
 export default function Home() {
   return (
-    <WorkspaceContextProvider>
-      <SessionProvider>
-        <Navbar />
-        <DataContextProvider>
-          <SelectionContextProvider>
-            <MemoryContextProvider>
-              <SearchContextProvider>
-                <DndProvider backend={HTML5Backend}>
-                  <Main />
-                </DndProvider>
-              </SearchContextProvider>
-            </MemoryContextProvider>
-          </SelectionContextProvider>
-        </DataContextProvider>
-        <Footer />
-      </SessionProvider>
-    </WorkspaceContextProvider>
+    <>
+      <GoogleAnalytics />
+      <WorkspaceContextProvider>
+        <SessionProvider>
+          <Navbar />
+          <DataContextProvider>
+            <SelectionContextProvider>
+              <MemoryContextProvider>
+                <SearchContextProvider>
+                  <DndProvider backend={HTML5Backend}>
+                    <Main />
+                  </DndProvider>
+                </SearchContextProvider>
+              </MemoryContextProvider>
+            </SelectionContextProvider>
+          </DataContextProvider>
+          <Footer />
+        </SessionProvider>
+      </WorkspaceContextProvider>
+    </>
   );
 }
