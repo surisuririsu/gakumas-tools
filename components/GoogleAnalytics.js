16f94f0c616a3c111fa8dd805b7145f2991e94e1
risりす <surisuririsu@gmail.com>
2024-07-27 22:53:09 -0400

    Add GA

---
+++ components/GoogleAnalytics.js
@@ -0,0 +1,21 @@
+import Script from "next/script";
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-D4SNMY9VJP"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-D4SNMY9VJP');
+        `}
+      </Script>
+    </>
+  );
+}
