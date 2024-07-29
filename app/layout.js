4aaa535bd4f40b19a63ace027074b3af5759a8b8
risりす <surisuririsu@gmail.com>
2024-07-29 11:15:55 -0400

    viewport scale

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,12 @@ export const metadata = {
     "Tools for playing Gakumas. Calculate the exam score required to achieve produce ranks, view P-item and skill card information, create your ideal contest loadouts, and more.",
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
