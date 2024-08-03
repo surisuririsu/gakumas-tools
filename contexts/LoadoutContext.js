60e3a8b1e183ef91ae74412d0c2cbd68972cb76f
risりす <surisuririsu@gmail.com>
2024-08-03 05:37:48 -0400

    Loadout skill card groups with functionality similar to contest planner

--- contexts/LoadoutContext.js
+++ contexts/LoadoutContext.js
@@ -1,8 +1,10 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import SelectionContext from "@/contexts/SelectionContext";
 
 const LoadoutContext = createContext();
 
 export function LoadoutContextProvider({ children }) {
+  const { setSelectedEntity } = useContext(SelectionContext);
   const [plan, setPlan] = useState("sense");
   const [idolId, setIdolId] = useState(1);
   const [pItemIds, setPItemIds] = useState([0, 0, 0, 0]);
@@ -23,6 +25,35 @@ export function LoadoutContextProvider({ children }) {
     });
   }
 
+  const insertSkillCardIdGroup = (groupIndex) => {
+    setSkillCardIdGroups((cur) => {
+      const updatedSkillCardIds = [...cur];
+      updatedSkillCardIds.splice(groupIndex, 0, [0, 0, 0, 0, 0, 0]);
+      return updatedSkillCardIds;
+    });
+    setSelectedEntity(null);
+  };
+
+  const deleteSkillCardIdGroup = (groupIndex) => {
+    setSkillCardIdGroups((cur) => {
+      const updatedSkillCardIds = [...cur];
+      updatedSkillCardIds.splice(groupIndex, 1);
+      return updatedSkillCardIds;
+    });
+    setSelectedEntity(null);
+  };
+
+  const swapSkillCardIdGroups = (groupIndexA, groupIndexB) => {
+    setSkillCardIdGroups((cur) => {
+      const updatedSkillCardIds = [...cur];
+      const temp = updatedSkillCardIds[groupIndexA];
+      updatedSkillCardIds[groupIndexA] = updatedSkillCardIds[groupIndexB];
+      updatedSkillCardIds[groupIndexB] = temp;
+      return updatedSkillCardIds;
+    });
+    setSelectedEntity(null);
+  };
+
   return (
     <LoadoutContext.Provider
       value={{
@@ -35,6 +66,9 @@ export function LoadoutContextProvider({ children }) {
         skillCardIdGroups,
         setSkillCardIdGroups,
         setSkillCardIds,
+        insertSkillCardIdGroup,
+        deleteSkillCardIdGroup,
+        swapSkillCardIdGroups,
       }}
     >
       {children}
