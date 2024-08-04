67c33cb965d6078002c7d25b94ff5dd5b0cd7429
risりす <surisuririsu@gmail.com>
2024-08-04 19:00:29 -0400

    Update footer links

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,11 +3,15 @@ import styles from "./Footer.module.scss";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
+      <a href="https://wikiwiki.jp/gakumas/" target="_blank">
+        Gakumas Contest Wiki
+      </a>
       <span>
         Made with ☕ by{" "}
-        <a href="https://www.ris.moe" target="_blank">
+        <a href="http://www.ris.moe" target="_blank">
           risりす
-        </a>
+        </a>{" "}
+        with research from the Gakumas contest community
       </span>
       <a
         href="https://github.com/surisuririsu/gakumas-tools/issues/new"
