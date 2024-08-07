0146bee8003be7600336541783e3365e49c17a8f
risりす <surisuririsu@gmail.com>
2024-08-07 14:50:51 -0400

    Add ability to set negative target skill card slots

--- components/TargetSkillCards/TargetSkillCards.js
+++ components/TargetSkillCards/TargetSkillCards.js
@@ -1,57 +1,74 @@
 import React, { useContext } from "react";
-import { FaCirclePlus } from "react-icons/fa6";
+import { FaCirclePlus, FaCircleMinus } from "react-icons/fa6";
 import EntityIcon from "@/components/EntityIcon";
 import MemoryCalculatorContext from "@/contexts/MemoryCalculatorContext";
 import { EntityTypes } from "@/utils/entities";
 import styles from "./TargetSkillCards.module.scss";
 
 export default function TargetSkillCards({ idolId }) {
-  const { targetSkillCardIds, alternateSkillCardIds, addAlternateSkillCards } =
-    useContext(MemoryCalculatorContext);
+  const {
+    targetSkillCardIds,
+    alternateSkillCardIds,
+    addAlternateSkillCards,
+    targetNegations,
+    setNegation,
+  } = useContext(MemoryCalculatorContext);
 
   return (
     <div className={styles.targetSkillCards}>
       {targetSkillCardIds.map((skillCardId, index) => (
-        <div
-          key={`${index}_${skillCardId}`}
-          className={`${styles.orGroup} ${
-            alternateSkillCardIds[index]?.length ? styles.hasMultiple : ""
-          }`}
-        >
-          <EntityIcon
-            type={EntityTypes.SKILL_CARD}
-            id={skillCardId}
-            region="memoryCalculator:target"
-            index={index}
-            idolId={idolId}
-          />
-
-          {alternateSkillCardIds[index]?.map((altSkillCardId, altIndex) => (
-            <React.Fragment
-              key={`${index}_${altIndex}_${skillCardId}_${altSkillCardId}`}
-            >
-              <span className={styles.or}>OR</span>
-              <EntityIcon
-                type={EntityTypes.SKILL_CARD}
-                id={altSkillCardId}
-                region="memoryCalculator:alternate"
-                index={index * 10 + altIndex}
-                idolId={idolId}
-              />
-            </React.Fragment>
-          ))}
-
-          {alternateSkillCardIds[index]?.length != 10 &&
-            alternateSkillCardIds[index]?.[
-              alternateSkillCardIds[index].length - 1
-            ] != 0 && (
-              <button
-                className={styles.plus}
-                onClick={() => addAlternateSkillCards(index)}
-              >
-                <FaCirclePlus />
-              </button>
+        <div key={`${index}_${skillCardId}`} className={styles.slot}>
+          <button
+            className={styles.minus}
+            onClick={() => setNegation(index, !targetNegations[index])}
+          >
+            {targetNegations[index] ? (
+              <span className={styles.not}>NOT</span>
+            ) : (
+              <FaCircleMinus />
             )}
+          </button>
+          <div
+            key={`${index}_${skillCardId}`}
+            className={`${styles.orGroup} ${
+              alternateSkillCardIds[index]?.length ? styles.hasMultiple : ""
+            }`}
+          >
+            <EntityIcon
+              type={EntityTypes.SKILL_CARD}
+              id={skillCardId}
+              region="memoryCalculator:target"
+              index={index}
+              idolId={idolId}
+            />
+
+            {alternateSkillCardIds[index]?.map((altSkillCardId, altIndex) => (
+              <React.Fragment
+                key={`${index}_${altIndex}_${skillCardId}_${altSkillCardId}`}
+              >
+                <span className={styles.or}>OR</span>
+                <EntityIcon
+                  type={EntityTypes.SKILL_CARD}
+                  id={altSkillCardId}
+                  region="memoryCalculator:alternate"
+                  index={index * 10 + altIndex}
+                  idolId={idolId}
+                />
+              </React.Fragment>
+            ))}
+
+            {alternateSkillCardIds[index]?.length != 10 &&
+              alternateSkillCardIds[index]?.[
+                alternateSkillCardIds[index].length - 1
+              ] != 0 && (
+                <button
+                  className={styles.plus}
+                  onClick={() => addAlternateSkillCards(index)}
+                >
+                  <FaCirclePlus />
+                </button>
+              )}
+          </div>
         </div>
       ))}
     </div>
