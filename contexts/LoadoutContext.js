87569a22533dbbee9f77875417ae06557c786cb0
risりす <surisuririsu@gmail.com>
2024-08-08 16:15:41 -0400

    Enable loading memories into loadout

--- contexts/LoadoutContext.js
+++ contexts/LoadoutContext.js
@@ -1,8 +1,13 @@
+import { useContext } from "react";
 import { createContext, useState } from "react";
+import DataContext from "@/contexts/DataContext";
 
 const LoadoutContext = createContext();
 
 export function LoadoutContextProvider({ children }) {
+  const { memories } = useContext(DataContext);
+  const [memoryIds, setMemoryIds] = useState([null, null]);
+  const [params, setParams] = useState([null, null, null, null]);
   const [pItemIds, setPItemIds] = useState([0, 0, 0, 0]);
   const [skillCardIdGroups, setSkillCardIdGroups] = useState([
     [0, 0, 0, 0, 0, 0],
@@ -47,9 +52,57 @@ export function LoadoutContextProvider({ children }) {
     });
   };
 
+  function setMemory(memory, index) {
+    const multiplier = index ? 0.2 : 1;
+
+    // If there is currently a memory in that slot, remove its params
+    if (memoryIds[index]) {
+      const curMemory = memories.find((mem) => mem._id == memoryIds[index]);
+      if (curMemory) {
+        setParams((curParams) =>
+          curParams.map(
+            (p, i) => (p || 0) - Math.floor(curMemory.params[i] * multiplier)
+          )
+        );
+      }
+    }
+
+    // Set memory
+    setMemoryIds((cur) => {
+      const next = [...cur];
+      next[index] = memory._id;
+      return next;
+    });
+    setParams((curParams) =>
+      curParams.map(
+        (p, i) => (p || 0) + Math.floor(memory.params[i] * multiplier)
+      )
+    );
+    if (index == 0) {
+      setPItemIds(memory.pItemIds);
+    }
+    setSkillCardIdGroups((cur) => {
+      cur[index] = memory.skillCardIds;
+      return cur;
+    });
+  }
+
+  function clear() {
+    setMemoryIds([null, null]);
+    setParams([null, null, null, null]);
+    setPItemIds([0, 0, 0, 0]);
+    setSkillCardIdGroups([
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+    ]);
+  }
+
   return (
     <LoadoutContext.Provider
       value={{
+        setMemory,
+        params,
+        setParams,
         pItemIds,
         setPItemIds,
         skillCardIdGroups,
@@ -58,6 +111,7 @@ export function LoadoutContextProvider({ children }) {
         insertSkillCardIdGroup,
         deleteSkillCardIdGroup,
         swapSkillCardIdGroups,
+        clear,
       }}
     >
       {children}
