572a203df1d4bddf22a8cde9adffdf0ce38d1ef3
risりす <surisuririsu@gmail.com>
2024-08-15 00:39:41 -0400

    Make tools pinnable on large screens

--- components/PinnedTools/PinnedTools.js
+++ components/PinnedTools/PinnedTools.js
@@ -1,22 +1,37 @@
 "use client";
 import { useContext } from "react";
+import { FaXmark } from "react-icons/fa6";
 import Dex from "@/components/Dex";
+import MemoryCalculator from "@/components/MemoryCalculator";
+import MemoryEditor from "@/components/MemoryEditor";
+import Memories from "@/components/Memories";
 import ProduceRankCalculator from "@/components/ProduceRankCalculator";
+import Simulator from "@/components/Simulator";
 import WorkspaceContext from "@/contexts/WorkspaceContext";
-import styles from "./PinnedTools.module.scss";
 import { TOOLS } from "@/utils/tools";
+import styles from "./PinnedTools.module.scss";
 
 export default function PinnedTools() {
   const { pinnedTools, unpin } = useContext(WorkspaceContext);
+
   return (
     <div className={styles.pinnedTools}>
       {pinnedTools.map((tool) => (
-        <div className={styles.tool}>
-          <div className={styles.toolHeader}>
-            <h2>{TOOLS[tool].icon}</h2>
+        <div className={styles.container}>
+          <div className={styles.header}>
+            <span>{TOOLS[tool].icon}</span>
+            <button onClick={() => unpin(tool)}>
+              <FaXmark />
+            </button>
           </div>
-          {tool == "produceRankCalculator" && <ProduceRankCalculator />}
-          {tool == "dex" && <Dex />}
+          <div className={styles.tool}>
+            {tool == "produceRankCalculator" && <ProduceRankCalculator />}
+            {tool == "dex" && <Dex />}
+            {tool == "memoryCalculator" && <MemoryCalculator />}
+            {tool == "memoryEditor" && <MemoryEditor />}
+            {tool == "memories" && <Memories />}
+            {tool == "simulator" && <Simulator />}
+          </div>
         </div>
       ))}
     </div>
