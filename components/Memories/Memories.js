fc60665bdcb14f733332c50208265679e6e97c3a
risりす <surisuririsu@gmail.com>
2024-08-20 02:57:07 -0400

    Swap out signin nudge for create memory nudge in memories logged out state

--- components/Memories/Memories.js
+++ components/Memories/Memories.js
@@ -1,15 +1,19 @@
 "use client";
 import { memo, useContext, useEffect, useState } from "react";
-import { useSession, signIn } from "next-auth/react";
+import { FaPen } from "react-icons/fa6";
+import { useSession } from "next-auth/react";
 import Button from "@/components/Button";
+import MemoryEditorModal from "@/components/MemoryEditorModal";
 import DataContext from "@/contexts/DataContext";
-import styles from "./Memories.module.scss";
+import ModalContext from "@/contexts/ModalContext";
 import MemoriesHeader from "./MemoriesHeader";
 import MemoriesList from "./MemoriesList";
+import styles from "./Memories.module.scss";
 
 function Memories() {
   const { status } = useSession();
   const { memories, fetchMemories } = useContext(DataContext);
+  const { setModal } = useContext(ModalContext);
   const [action, setAction] = useState(null);
   const [selectedMemories, setSelectedMemories] = useState({});
 
@@ -41,8 +45,8 @@ function Memories() {
 
       {status == "unauthenticated" && (
         <div className={styles.nudge}>
-          <Button onClick={() => signIn("discord")}>
-            Sign in with Discord to save/load memories
+          <Button onClick={() => setModal(<MemoryEditorModal />)}>
+            <FaPen /> Create a memory
           </Button>
         </div>
       )}
