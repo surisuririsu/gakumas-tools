c6332c356fb0cf3a6853c5126f1051502f28ac86
risりす <surisuririsu@gmail.com>
2024-08-20 09:09:41 -0400

    Better deck shuffling

--- utils/simulator.js
+++ utils/simulator.js
@@ -103,3 +103,20 @@ export function mergeResults(results) {
     scores,
   };
 }
+
+export function shuffle(arr) {
+  let shuffled = [...arr];
+  let currentIndex = shuffled.length;
+
+  while (currentIndex != 0) {
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [shuffled[currentIndex], shuffled[randomIndex]] = [
+      shuffled[randomIndex],
+      shuffled[currentIndex],
+    ];
+  }
+
+  return shuffled;
+}
