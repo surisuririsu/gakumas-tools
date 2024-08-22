24dd2a0839195c3e32e25e865b07ca5fc9c3a50f
risりす <surisuririsu@gmail.com>
2024-08-22 16:01:25 -0400

    Fix style of rehearsal parser result

--- components/Rehearsal/RehearsalTable.js
+++ components/Rehearsal/RehearsalTable.js
@@ -1,9 +1,8 @@
 import React from "react";
-import styles from "./Rehearsal.module.scss";
 
 export default function RehearsalTable({ data }) {
   return (
-    <table className={styles.result}>
+    <table>
       <thead>
         <tr>
           <th colSpan="3">ステージ1</th>
