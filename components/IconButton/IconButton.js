0cfbc5de9df9efb1eb887adea0be01978426f97d
risりす <surisuririsu@gmail.com>
2024-08-31 09:45:15 -0400

    Move footer content to user menu

--- components/IconButton/IconButton.js
+++ components/IconButton/IconButton.js
@@ -1,9 +1,15 @@
 import { memo } from "react";
+import Link from "next/link";
 import styles from "./IconButton.module.scss";
 
-function IconButton({ icon: Icon, onClick }) {
-  return (
-    <button className={styles.iconButton} onClick={onClick}>
+function IconButton({ icon: Icon, onClick, href, size = "medium" }) {
+  const className = `${styles.iconButton} ${styles[size]}`;
+  return href ? (
+    <Link className={className} href={href} target="_blank" onClick={onClick}>
+      <Icon />
+    </Link>
+  ) : (
+    <button className={className} onClick={onClick}>
       <Icon />
     </button>
   );
