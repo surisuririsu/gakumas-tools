9b57104e780dffb3709fc4db3c9f52fc09511122
risりす <surisuririsu@gmail.com>
2024-09-01 20:58:43 -0400

    Generate kafe simulator id mappings in a script so we don't need to include kafe sim in prod bundles

--- .pnp.cjs
+++ .pnp.cjs
@@ -1320,7 +1320,7 @@ const RAW_RUNTIME_STATE =
         "packageDependencies": [\
           ["@jest/console", "npm:29.7.0"],\
           ["@jest/types", "npm:29.6.3"],\
-          ["@types/node", "npm:22.5.1"],\
+          ["@types/node", "npm:22.5.2"],\
           ["chalk", "npm:4.1.2"],\
           ["jest-message-util", "npm:29.7.0"],\
           ["jest-util", "npm:29.7.0"],\
@@ -1346,7 +1346,7 @@ const RAW_RUNTIME_STATE =
           ["@jest/test-result", "npm:29.7.0"],\
           ["@jest/transform", "npm:29.7.0"],\
           ["@jest/types", "npm:29.6.3"],\
-          ["@types/node", "npm:22.5.1"],\
+          ["@types/node", "npm:22.5.2"],\
           ["@types/node-notifier", null],\
           ["ansi-escapes", "npm:4.3.2"],\
           ["chalk", "npm:4.1.2"],\
@@ -1386,7 +1386,7 @@ const RAW_RUNTIME_STATE =
           ["@jest/environment", "npm:29.7.0"],\
           ["@jest/fake-timers", "npm:29.7.0"],\
           ["@jest/types", "npm:29.6.3"],\
-          ["@types/node", "npm:22.5.1"],\
+          ["@types/node", "npm:22.5.2"],\
           ["jest-mock", "npm:29.7.0"]\
         ],\
         "linkType": "HARD"\
@@ -1420,7 +1420,7 @@ const RAW_RUNTIME_STATE =
           ["@jest/fake-timers", "npm:29.7.0"],\
           ["@jest/types", "npm:29.6.3"],\
           ["@sinonjs/fake-timers", "npm:10.3.0"],\
-          ["@types/node", "npm:22.5.1"],\
+          ["@types/node", "npm:22.5.2"],\
           ["jest-message-util", "npm:29.7.0"],\
           ["jest-mock", "npm:29.7.0"],\
           ["jest-util", "npm:29.7.0"]\
@@ -1459,7 +1459,7 @@ const RAW_RUNTIME_STATE =
           ["@jest/transform", "npm:29.7.0"],\
           ["@jest/types", "npm:29.6.3"],\
           ["@jridgewell/trace-mapping", "npm:0.3.25"],\
-          ["@types/node", "npm:22.5.1"],\
+          ["@types/node", "npm:22.5.2"],\
           ["@types/node-notifier", null],\
           ["chalk", "npm:4.1.2"],\
           ["collect-v8-coverage", "npm:1.0.2"],\
@@ -1567,7 +1567,7 @@ const RAW_RUNTIME_STATE =
           ["@jest/schemas", "npm:29.6.3"],\
           ["@types/istanbul-lib-coverage", "npm:2.0.6"],\
           ["@types/istanbul-reports", "npm:3.0.4"],\
-          ["@types/node", "npm:22.5.1"],\
+          ["@types/node", "npm:22.5.2"],\
           ["@types/yargs", "npm:17.0.33"],\
           ["chalk", "npm:4.1.2"]\
         ],\
@@ -2056,7 +2056,7 @@ const RAW_RUNTIME_STATE =
         "packageLocation": "../../../AppData/Local/Yarn/Berry/cache/@types-graceful-fs-npm-4.1.9-ebd697fe83-10c0.zip/node_modules/@types/graceful-fs/",\
         "packageDependencies": [\
           ["@types/graceful-fs", "npm:4.1.9"],\
-          ["@types/node", "npm:22.5.1"]\
+          ["@types/node", "npm:22.5.2"]\
         ],\
         "linkType": "HARD"\
       }]\
@@ -2095,7 +2095,7 @@ const RAW_RUNTIME_STATE =
         "packageLocation": "../../../AppData/Local/Yarn/Berry/cache/@types-jsdom-npm-20.0.1-5bb899e006-10c0.zip/node_modules/@types/jsdom/",\
         "packageDependencies": [\
           ["@types/jsdom", "npm:20.0.1"],\
-          ["@types/node", "npm:22.5.1"],\
+          ["@types/node", "npm:22.5.2"],\
           ["@types/tough-cookie", "npm:4.0.5"],\
           ["parse5", "npm:7.1.2"]\
         ],\
@@ -2112,10 +2112,10 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["@types/node", [\
-      ["npm:22.5.1", {\
-        "packageLocation": "../../../AppData/Local/Yarn/Berry/cache/@types-node-npm-22.5.1-684f129d7d-10c0.zip/node_modules/@types/node/",\
+      ["npm:22.5.2", {\
+        "packageLocation": "../../../AppData/Local/Yarn/Berry/cache/@types-node-npm-22.5.2-430cf418f9-10c0.zip/node_modules/@types/node/",\
         "packageDependencies": [\
-          ["@types/node", "npm:22.5.1"],\
+          ["@types/node", "npm:22.5.2"],\
           ["undici-types", "npm:6.19.8"]\
         ],\
         "linkType": "HARD"\
@@ -2315,7 +2315,7 @@ const RAW_RUNTIME_STATE =
           ["@vue/shared", "npm:3.4.38"],\
           ["estree-walker", "npm:2.0.2"],\
           ["magic-string", "npm:0.30.11"],\
-          ["postcss", "npm:8.4.42"],\
+          ["postcss", "npm:8.4.43"],\
           ["source-map-js", "npm:1.2.0"]\
         ],\
         "linkType": "HARD"\
@@ -5814,7 +5814,7 @@ const RAW_RUNTIME_STATE =
           ["@jest/expect", "npm:29.7.0"],\
           ["@jest/test-result", "npm:29.7.0"],\
           ["@jest/types", "npm:29.6.3"],\
-          ["@types/node", "npm:22.5.1"],\
+          ["@types/node", "npm:22.5.2"],\
           ["chalk", "npm:4.1.2"],\
           ["co", "npm:4.6.0"],\
           ["dedent", "virtual:f7679858c638e2e5ade31901dd2b1e5007918fdc7d84fefb11f4200f46ba2e43b9d662fb793507b517bb1e725144e51f6d68f60f9f6100fd52144f042f58f0bc#npm:1.5.3"],\
@@ -5919,7 +5919,7 @@ const RAW_RUNTIME_STATE =
           ["@babel/core", "npm:7.25.2"],\
           ["@jest/test-sequencer", "npm:29.7.0"],\
           ["@jest/types", "npm:29.6.3"],\
-          ["@types/node", "npm:22.5.1"],\
+          ["@types/node", "npm:22.5.2"],\
           ["@types/ts-node", null],\
           ["babel-jest", "virtual:ff6bc6e0c36e2ca37d0c082ca817756e39e89dfa9b97d437f9b2e8439f146aaafbb7e3f4e205e680e749897b7da0655c15b48003116f89bc4b5c31678157ac7e#npm:29.7.0"],\
           ["chalk", "npm:4.1.2"],\
@@ -6004,7 +6004,7 @@ const RAW_RUNTIME_STATE =
           ["@jest/types", "npm:29.6.3"],\
           ["@types/canvas", null],\
           ["@types/jsdom", "npm:20.0.1"],\
-          ["@types/node", "npm:22.5.1"],\
+          ["@types/node", "npm:22.5.2"],\
           ["canvas", null],\
           ["jest-mock", "npm:29.7.0"],\
           ["jest-util", "npm:29.7.0"],\
@@ -6025,7 +6025,7 @@ const RAW_RUNTIME_STATE =
           ["@jest/environment", "npm:29.7.0"],\
           ["@jest/fake-timers", "npm:29.7.0"],\
           ["@jest/types", "npm:29.6.3"],\
-          ["@types/node", "npm:22.5.1"],\
+          ["@types/node", "npm:22.5.2"],\
           ["jest-mock", "npm:29.7.0"],\
           ["jest-util", "npm:29.7.0"]\
         ],\
@@ -6048,7 +6048,7 @@ const RAW_RUNTIME_STATE =
           ["jest-haste-map", "npm:29.7.0"],\
           ["@jest/types", "npm:29.6.3"],\
           ["@types/graceful-fs", "npm:4.1.9"],\
-          ["@types/node", "npm:22.5.1"],\
+          ["@types/node", "npm:22.5.2"],\
           ["anymatch", "npm:3.1.3"],\
           ["fb-watchman", "npm:2.0.2"],\
           ["fsevents", "patch:fsevents@npm%3A2.3.3#optional!builtin<compat/fsevents>::version=2.3.3&hash=df0bf1"],\
@@ -6110,7 +6110,7 @@ const RAW_RUNTIME_STATE =
         "packageDependencies": [\
           ["jest-mock", "npm:29.7.0"],\
           ["@jest/types", "npm:29.6.3"],\
-          ["@types/node", "npm:22.5.1"],\
+          ["@types/node", "npm:22.5.2"],\
           ["jest-util", "npm:29.7.0"]\
         ],\
         "linkType": "HARD"\
@@ -6186,7 +6186,7 @@ const RAW_RUNTIME_STATE =
           ["@jest/test-result", "npm:29.7.0"],\
           ["@jest/transform", "npm:29.7.0"],\
           ["@jest/types", "npm:29.6.3"],\
-          ["@types/node", "npm:22.5.1"],\
+          ["@types/node", "npm:22.5.2"],\
           ["chalk", "npm:4.1.2"],\
           ["emittery", "npm:0.13.1"],\
           ["graceful-fs", "npm:4.2.11"],\
@@ -6218,7 +6218,7 @@ const RAW_RUNTIME_STATE =
           ["@jest/test-result", "npm:29.7.0"],\
           ["@jest/transform", "npm:29.7.0"],\
           ["@jest/types", "npm:29.6.3"],\
-          ["@types/node", "npm:22.5.1"],\
+          ["@types/node", "npm:22.5.2"],\
           ["chalk", "npm:4.1.2"],\
           ["cjs-module-lexer", "npm:1.4.0"],\
           ["collect-v8-coverage", "npm:1.0.2"],\
@@ -6272,7 +6272,7 @@ const RAW_RUNTIME_STATE =
         "packageDependencies": [\
           ["jest-util", "npm:29.7.0"],\
           ["@jest/types", "npm:29.6.3"],\
-          ["@types/node", "npm:22.5.1"],\
+          ["@types/node", "npm:22.5.2"],\
           ["chalk", "npm:4.1.2"],\
           ["ci-info", "npm:3.9.0"],\
           ["graceful-fs", "npm:4.2.11"],\
@@ -6303,7 +6303,7 @@ const RAW_RUNTIME_STATE =
           ["jest-watcher", "npm:29.7.0"],\
           ["@jest/test-result", "npm:29.7.0"],\
           ["@jest/types", "npm:29.6.3"],\
-          ["@types/node", "npm:22.5.1"],\
+          ["@types/node", "npm:22.5.2"],\
           ["ansi-escapes", "npm:4.3.2"],\
           ["chalk", "npm:4.1.2"],\
           ["emittery", "npm:0.13.1"],\
@@ -6318,7 +6318,7 @@ const RAW_RUNTIME_STATE =
         "packageLocation": "../../../AppData/Local/Yarn/Berry/cache/jest-worker-npm-29.7.0-4d3567fed6-10c0.zip/node_modules/jest-worker/",\
         "packageDependencies": [\
           ["jest-worker", "npm:29.7.0"],\
-          ["@types/node", "npm:22.5.1"],\
+          ["@types/node", "npm:22.5.2"],\
           ["jest-util", "npm:29.7.0"],\
           ["merge-stream", "npm:2.0.0"],\
           ["supports-color", "npm:8.1.1"]\
@@ -7601,10 +7601,10 @@ const RAW_RUNTIME_STATE =
         ],\
         "linkType": "HARD"\
       }],\
-      ["npm:8.4.42", {\
-        "packageLocation": "../../../AppData/Local/Yarn/Berry/cache/postcss-npm-8.4.42-ae7ce97805-10c0.zip/node_modules/postcss/",\
+      ["npm:8.4.43", {\
+        "packageLocation": "../../../AppData/Local/Yarn/Berry/cache/postcss-npm-8.4.43-3b7e013e4e-10c0.zip/node_modules/postcss/",\
         "packageDependencies": [\
-          ["postcss", "npm:8.4.42"],\
+          ["postcss", "npm:8.4.43"],\
           ["nanoid", "npm:3.3.7"],\
           ["picocolors", "npm:1.0.1"],\
           ["source-map-js", "npm:1.2.0"]\
--- utils/kafeSimulator.js
+++ utils/kafeSimulator.js
@@ -1,90 +1,9 @@
-import { PItems, SkillCards, Stages } from "gakumas-data";
+import { PItems, SkillCards } from "gakumas-data";
 import { IdolConfig } from "gakumas-engine";
-import { ContestData } from "gakumas_contest_simulator/scripts/simulator/data/contestData";
-import { PIdolData as SimulatorPIdols } from "gakumas_contest_simulator/scripts/simulator/data/pIdolData";
-import { PItemData as SimulatorPItems } from "gakumas_contest_simulator/scripts/simulator/data/pItemData";
-import { SkillCardData as SimulatorSkillCards } from "gakumas_contest_simulator/scripts/simulator/data/skillCardData";
-
-export const KAFE_CONTEST_STAGES = ContestData.getAll().reduce((acc, cur) => {
-  const stages = cur.stages
-    .map((stage, stageIdx) => {
-      const normalizedName = `${cur.name} ${stage.name}`.normalize("NFKC");
-      const match = normalizedName.match(/第(\d+)期コンテスト.*ステージ(\d+)/);
-      if (!match) return null;
-      return {
-        id: `${cur.id}:${stageIdx}`,
-        seasonStage: `${match[1]}-${match[2]}`,
-      };
-    })
-    .filter((s) => s);
-  return stages.concat(acc);
-}, []);
-
-const KAFE_ITEM_IDS_BY_NAME = SimulatorPItems.getAll().reduce(
-  (acc, cur) => ({
-    ...acc,
-    [cur.name]: cur.id,
-  }),
-  {}
-);
-
-const KAFE_CARD_IDS_BY_NAME = SimulatorSkillCards.getAll().reduce(
-  (acc, cur) => ({
-    ...acc,
-    [cur.name]: cur.id,
-  }),
-  {}
-);
-
-const KAFE_STAGES_BY_SEASON_STAGE = KAFE_CONTEST_STAGES.reduce(
-  (acc, cur) => ({
-    ...acc,
-    [cur.seasonStage]: cur.id,
-  }),
-  {}
-);
-
-const KAFE_P_IDOL_IDS_BY_KAFE_CARD_ID = SimulatorPIdols.getAll().reduce(
-  (acc, cur) => ({
-    ...acc,
-    [cur.unique_skillCard_id]: cur.id,
-    [cur.unique_skillCard_id + 1]: cur.id,
-  }),
-  {}
-);
-
-const KAFE_ITEM_NAME_FIXUPS = {
-  140: "コール＆レスポンス",
-  141: "コール＆レスポンス+",
-  148: "２００％スマイル",
-  149: "２００％スマイル+",
-  280: "ＰＯＷ！",
-  281: "ＰＯＷ！+",
-};
-
-const KAFE_ITEM_MAP = PItems.getAll().reduce((acc, cur) => {
-  acc[cur.id] = KAFE_ITEM_IDS_BY_NAME[cur.name] || -1;
-  return acc;
-}, {});
-
-const KAFE_CARD_MAP = SkillCards.getAll().reduce((acc, cur) => {
-  acc[cur.id] =
-    KAFE_CARD_IDS_BY_NAME[KAFE_ITEM_NAME_FIXUPS[cur.id] || cur.name] || -1;
-  return acc;
-}, {});
-
-const KAFE_STAGE_MAP = Stages.getAll().reduce((acc, cur) => {
-  acc[cur.id] =
-    KAFE_STAGES_BY_SEASON_STAGE[`${cur.season}-${cur.stage}`] || "-1:-1";
-  return acc;
-}, {});
-
-const MISMATCHED_ITEMS = PItems.getAll().filter(
-  (item) => KAFE_ITEM_MAP[item.id] === -1
-);
-const MISMATCHED_CARDS = SkillCards.getAll().filter(
-  (card) => KAFE_CARD_MAP[card.id] === -1
-);
+import KAFE_CARD_MAP from "@/generated/kafeCardMap.json";
+import KAFE_ITEM_MAP from "@/generated/kafeItemMap.json";
+import KAFE_P_IDOL_IDS_BY_KAFE_CARD_ID from "@/generated/kafePIdolIdsByKafeCardId.json";
+import KAFE_STAGE_MAP from "@/generated/kafeStageMap.json";
 
 const KAFE_URL_BASE = "https://katabami83.github.io/gakumas_contest_simulator";
 
@@ -95,7 +14,6 @@ export function generateKafeUrl(
   pItemIds,
   skillCardIdGroups
 ) {
-  const kafeStage = KAFE_STAGE_MAP[stage.id];
   const idolConfig = new IdolConfig(
     params,
     supportBonus,
@@ -103,12 +21,21 @@ export function generateKafeUrl(
     skillCardIdGroups,
     stage
   );
+
+  // Convert stage id
+  const kafeStage = KAFE_STAGE_MAP[stage.id];
+
+  // Convert parameters
   const status = Object.values(idolConfig.typeMultipliers)
     .map((s) => Math.round(s * 100))
     .concat(idolConfig.params.stamina);
+
+  // Convert p-item ids
   const itemSimulatorIds = pItemIds
     .filter((i) => PItems.getById(i) && PItems.getById(i).sourceType != "pIdol")
     .map((i) => KAFE_ITEM_MAP[i] || -1);
+
+  // Convert skill card ids
   const groups = skillCardIdGroups.slice();
   while (groups.length < 2) {
     groups.push([0, 0, 0, 0, 0, 0]);
@@ -135,6 +62,7 @@ export function generateKafeUrl(
       )
     );
 
+  // Build search query
   const simulatorParams = new URLSearchParams();
   simulatorParams.set("contest_stage", kafeStage);
   simulatorParams.set(
@@ -152,6 +80,3 @@ export function generateKafeUrl(
 
   return `${KAFE_URL_BASE}/?${simulatorParams.toString()}`;
 }
-
-if (MISMATCHED_ITEMS.length) console.log("Mismatched items", MISMATCHED_ITEMS);
-if (MISMATCHED_CARDS.length) console.log("Mismatched cards", MISMATCHED_CARDS);
