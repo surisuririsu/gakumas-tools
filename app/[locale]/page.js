78ba9d9243dcc23fa0967acfe270ebccbd5ac794
risりす <surisuririsu@gmail.com>
2024-09-07 18:17:46 -0400

    Add WebSite structured data

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -6,7 +6,23 @@ export async function generateMetadata({ params: { locale } }) {
   return await generateMetadataForTool("home", locale);
 }
 
+const JSON_LD = {
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  name: "Gakumas Tools",
+  alternateName: "gktools.ris.moe",
+  url: "https://gktools.ris.moe/",
+};
+
 export default function WelcomePage({ params: { locale } }) {
   unstable_setRequestLocale(locale);
-  return <Welcome />;
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(JSON_LD) }}
+      />
+      <Welcome />
+    </>
+  );
 }
