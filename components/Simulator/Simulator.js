b406a0c15fdaa85d1bcd08eaa9cb7666abd3b641
risりす <surisuririsu@gmail.com>
2024-09-19 16:35:51 -0400

    Revert "Update messaging"
    This reverts commit 6d32dbea1f25d8e47c3c9f949d1bdf021dd077ab.

--- components/Simulator/Simulator.js
+++ components/Simulator/Simulator.js
@@ -20,12 +20,6 @@ import SimulatorButtons from "./SimulatorButtons";
 import SimulatorSubTools from "./SimulatorSubTools";
 import styles from "./Simulator.module.scss";
 
-IdolConfig.prototype.getTypeMultipliers = (params) => ({
-  vocal: params.vocal / 100,
-  dance: params.dance / 100,
-  visual: params.visual / 100,
-});
-
 export default function Simulator() {
   const t = useTranslations("Simulator");
 
@@ -131,7 +125,7 @@ export default function Simulator() {
       <div className={styles.configurator}>
         <StageSelect />
 
-        {/* <div className={styles.supportBonusInput}>
+        <div className={styles.supportBonusInput}>
           <label>{t("supportBonus")}</label>
           <Input
             type="number"
@@ -140,11 +134,7 @@ export default function Simulator() {
               setSupportBonus(parseFloat((value / 100).toFixed(4)))
             }
           />
-        </div> */}
-        <span>
-          Note: Due to type multiplier calculation change, enter the percents
-          directly.
-        </span>
+        </div>
         <div className={styles.params}>
           <ParametersInput
             parameters={loadout.params}
@@ -152,14 +142,14 @@ export default function Simulator() {
             withStamina
             max={10000}
           />
-          <div className={styles.typeMultipliers}>
+          {/* <div className={styles.typeMultipliers}>
             {Object.keys(idolConfig.typeMultipliers).map((param) => (
               <div key={param}>
                 {Math.round(idolConfig.typeMultipliers[param] * 100)}%
               </div>
             ))}
             <div />
-          </div>
+          </div> */}
         </div>
         <StagePItems
           pItemIds={loadout.pItemIds}
