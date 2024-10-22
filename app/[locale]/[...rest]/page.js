f984225c8a2a1e3621c32f5b34405a4a519863ed
risりす <surisuririsu@gmail.com>
2024-10-22 15:44:47 -0400

    Update error pages with illo

--- app/[locale]/[...rest]/page.js
+++
@@ -1,6 +0,0 @@
-"use client";
-import Error from "next/error";
-
-export default function RestPage() {
-  return <Error statusCode={404} />;
-}
---
+++ app/[locale]/error.js
@@ -0,0 +1,6 @@
+"use client";
+import Error from "@/components/Error";
+
+export default function NotFound() {
+  return <Error />;
+}
