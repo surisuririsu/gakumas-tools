e0ea01bc84975cb83e66b31733ac17204060fff4
risりす <surisuririsu@gmail.com>
2024-10-22 15:44:47 -0400

    Update error pages with illo

---
+++ components/Error/Error.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import styles from "./Error.module.scss";
+
+export default function Error({ code }) {
+  return (
+    <div className={styles.error}>
+      <h2>{code == 404 ? "404 Not Found" : "Error"}</h2>
+      <div className={styles.wrapper}>
+        <Image
+          src={code == 404 ? "/errors/not_found.jpg" : "/errors/generic.jpg"}
+          fill
+        />
+      </div>
+    </div>
+  );
+}
