6ac3c401362183363be90e3bac732f59fe7d980b
risりす <surisuririsu@gmail.com>
2024-10-31 17:32:22 -0400

    Fix preview image

--- utils/data.js
+++ utils/data.js
@@ -5,31 +5,39 @@ export const GH_PAGES_BASE_URL = "https://gkimg.ris.moe";
 
 let initialized = false;
 
-if (!initialized && process.env.NEXT_PUBLIC_USE_GH_PAGES == "true") {
-  Idols.getAll().forEach((idol) => {
-    idol.getIcon = () => `${GH_PAGES_BASE_URL}/idols/${idol.id}.png`;
-  });
-  PIdols.getAll().forEach((pIdol) => {
-    pIdol.getIcon = () => `${GH_PAGES_BASE_URL}/p_idols/${pIdol.id}.webp`;
-  });
+if (!initialized) {
   PItems.getAll().forEach((pItem) => {
     pItem._getIcon = pItem.getIcon;
-    pItem.getIcon = () => `${GH_PAGES_BASE_URL}/p_items/icons/${pItem.id}.webp`;
-    pItem.details = `${GH_PAGES_BASE_URL}/p_items/details/${pItem.id}.webp`;
   });
   SkillCards.getAll().forEach((skillCard) => {
     skillCard._getIcon = skillCard.getIcon;
-    skillCard.getIcon = (idolId = 1) => {
-      const fileName =
-        `${skillCard.id}_${idolId}` in SKILL_CARD_ICONS
-          ? `${skillCard.id}_${idolId}`
-          : skillCard.id;
-      return `${GH_PAGES_BASE_URL}/skill_cards/icons/${fileName}.webp`;
-    };
-    skillCard.details = `${GH_PAGES_BASE_URL}/skill_cards/details/${skillCard.id}.webp`;
   });
-}
 
-initialized = true;
+  if (process.env.NEXT_PUBLIC_USE_GH_PAGES == "true") {
+    Idols.getAll().forEach((idol) => {
+      idol.getIcon = () => `${GH_PAGES_BASE_URL}/idols/${idol.id}.png`;
+    });
+    PIdols.getAll().forEach((pIdol) => {
+      pIdol.getIcon = () => `${GH_PAGES_BASE_URL}/p_idols/${pIdol.id}.webp`;
+    });
+    PItems.getAll().forEach((pItem) => {
+      pItem.getIcon = () =>
+        `${GH_PAGES_BASE_URL}/p_items/icons/${pItem.id}.webp`;
+      pItem.details = `${GH_PAGES_BASE_URL}/p_items/details/${pItem.id}.webp`;
+    });
+    SkillCards.getAll().forEach((skillCard) => {
+      skillCard.getIcon = (idolId = 1) => {
+        const fileName =
+          `${skillCard.id}_${idolId}` in SKILL_CARD_ICONS
+            ? `${skillCard.id}_${idolId}`
+            : skillCard.id;
+        return `${GH_PAGES_BASE_URL}/skill_cards/icons/${fileName}.webp`;
+      };
+      skillCard.details = `${GH_PAGES_BASE_URL}/skill_cards/details/${skillCard.id}.webp`;
+    });
+  }
+
+  initialized = true;
+}
 
 export * from "gakumas-data";
