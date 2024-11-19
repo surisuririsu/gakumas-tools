865ec565ffb6f9fad47880fab514e4768d563b23
risりす <surisuririsu@gmail.com>
2024-11-19 09:09:04 +0900

    Adjust simulator strategy for anomaly

--- components/Simulator/Simulator.js
+++ components/Simulator/Simulator.js
@@ -213,7 +213,7 @@ export default function Simulator() {
             href="https://github.com/surisuririsu/gakumas-tools/blob/master/simulator/CHANGELOG.md"
             target="_blank"
           >
-            {t("lastUpdated")}: 2024-11-18
+            {t("lastUpdated")}: 2024-11-19
           </a>
         </div>
       </div>
