949f3c288f79ba03de9b3ec2f270357c02cd2e90
risりす <surisuririsu@gmail.com>
2024-12-24 16:10:33 -0500

    Remove warning message, update default stage

--- components/Simulator/Simulator.js
+++ components/Simulator/Simulator.js
@@ -117,10 +117,10 @@ export default function Simulator() {
   return (
     <div id="simulator_loadout" className={styles.loadoutEditor}>
       <div className={styles.configurator}>
-        <>
-          ※ Season 15 preview is based only on the announced p-items and
+        {/* <>
+          ※ Season 16 preview is based only on the announced p-items and
           criteria. Turn types are not yet known.
-        </>
+        </> */}
         <StageSelect />
         {stage.type == "event" ? (
           t("enterPercents")
