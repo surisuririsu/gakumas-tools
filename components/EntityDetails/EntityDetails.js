83366fb0204c8ed3c84bc3672a6588264903ab94
risりす <surisuririsu@gmail.com>
2025-01-28 23:45:01 -0500

    Add customization info to dex

--- components/EntityDetails/EntityDetails.js
+++ components/EntityDetails/EntityDetails.js
@@ -2,6 +2,7 @@ import { memo } from "react";
 import { FaRegRectangleList } from "react-icons/fa6";
 import Image from "@/components/Image";
 import { ENTITY_DATA_BY_TYPE } from "@/utils/entities";
+import AvailableCustomizations from "./AvailableCustomizations";
 import styles from "./EntityDetails.module.scss";
 
 function EntityDetails({ type, id }) {
@@ -10,18 +11,23 @@ function EntityDetails({ type, id }) {
   return (
     <div className={styles.entityDetails}>
       {entity ? (
-        <Image
-          src={entity.details}
-          alt={entity.name}
-          fill
-          sizes="400px"
-          draggable={false}
-        />
+        <>
+          <div className={styles.imageWrapper}>
+            <Image
+              src={entity.details}
+              alt={entity.name}
+              fill
+              sizes="400px"
+              draggable={false}
+            />
+          </div>
+        </>
       ) : (
         <div className={styles.placeholder}>
           <FaRegRectangleList />
         </div>
       )}
+      <AvailableCustomizations entity={entity} />
     </div>
   );
 }
