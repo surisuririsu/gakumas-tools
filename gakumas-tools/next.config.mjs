83263a2f1af9544a59f61da5419734e513d411d1
risりす <surisuririsu@gmail.com>
2025-03-10 21:28:22 -0400

    Dockerize and build/push to docker hub

--- gakumas-tools/next.config.mjs
+++ gakumas-tools/next.config.mjs
@@ -42,6 +42,7 @@ const nextConfig = {
     });
     return config;
   },
+  output: "standalone",
 };
 
 export default withNextIntl(nextConfig);
--- gakumas-tools/utils/mongodb.js
+++ gakumas-tools/utils/mongodb.js
@@ -3,13 +3,13 @@ import { MongoClient } from "mongodb";
 const { MONGODB_URI, MONGODB_DB } = process.env;
 
 if (!MONGODB_URI) {
-  throw new Error(
+  console.warn(
     "Please define the MONGODB_URI environment variable inside .env.local"
   );
 }
 
 if (!MONGODB_DB) {
-  throw new Error(
+  console.warn(
     "Please define the MONGODB_DB environment variable inside .env.local"
   );
 }
