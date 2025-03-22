936983f9290c324f146f47db9b1b73f69d3a7790
risりす <surisuririsu@gmail.com>
2025-03-21 23:47:58 -0400

    misc

--- gakumas-tools/components/EntityIcon/TierIndicator.js
+++ gakumas-tools/components/EntityIcon/TierIndicator.js
@@ -2,10 +2,12 @@ import { useTranslations } from "next-intl";
 import styles from "./EntityIcon.module.scss";
 import c from "@/utils/classNames";
 
+const TIERED_RARITIES = ["R", "SR"];
+
 export default function TierIndicator({ skillCard }) {
   const t = useTranslations("EntityIcon");
   if (skillCard.sourceType != "produce") return null;
-  if (!["R", "SR"].includes(skillCard.rarity)) return null;
+  if (!TIERED_RARITIES.includes(skillCard.rarity)) return null;
   const tier = skillCard.unlockPlv > 2 ? "high" : "low";
   return (
     <div
