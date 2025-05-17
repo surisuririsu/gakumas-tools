d6427074d8571ebb53fda5894621563631fc64ea
risりす <surisuririsu@gmail.com>
2025-05-17 16:07:10 -0400

    Hide s24 note, update default stage

--- gakumas-tools/utils/simulator.js
+++ gakumas-tools/utils/simulator.js
@@ -8,7 +8,7 @@ import {
 import { deserializeIds, serializeIds } from "./ids";
 
 const DEFAULTS = {
-  stageId: "82",
+  stageId: "85",
   supportBonus: "0",
   params: "1500-1500-1500-50",
   pItemIds: "0-0-0",
