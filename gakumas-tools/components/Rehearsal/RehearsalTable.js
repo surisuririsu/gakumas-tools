4dcd87edc07da33fbd28e9d75bd284da2427802e
risりす <surisuririsu@gmail.com>
2025-06-19 07:33:19 +0900

    More graphs for rehearsal

--- gakumas-tools/components/Rehearsal/RehearsalTable.js
+++ gakumas-tools/components/Rehearsal/RehearsalTable.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { AiOutlineBarChart } from "react-icons/ai";
+import styles from "./Rehearsal.module.scss";
 
-export default function RehearsalTable({ data }) {
+export default function RehearsalTable({ data, selected, onChartClick }) {
   return (
     <table>
       <thead>
+        <tr className={styles.chartButtons}>
+          {[...Array(9)].map((_, i) => (
+            <th key={i}>
+              <button
+                className={selected == i ? styles.selected : null}
+                onClick={() => onChartClick(i)}
+              >
+                <AiOutlineBarChart />
+              </button>
+            </th>
+          ))}
+        </tr>
         <tr>
           <th colSpan="3">ステージ1</th>
           <th colSpan="3">ステージ2</th>
