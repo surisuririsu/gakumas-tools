fab9f1d40506a74c11866ed9f367cc3b864b849e
risりす <surisuririsu@gmail.com>
2025-07-03 05:45:19 -0400

    Add season 27 preview

--- gakumas-tools/components/Simulator/Simulator.js
+++ gakumas-tools/components/Simulator/Simulator.js
@@ -147,10 +147,10 @@ export default function Simulator() {
   return (
     <div id="simulator_loadout" className={styles.loadoutEditor}>
       <div className={styles.configurator}>
-        {/* <div>
-          ※ Season 26 preview is based only on the announced p-items and
+        <div>
+          ※ Season 27 preview is based only on the announced p-items and
           criteria. Turn types are not yet known.
-        </div> */}
+        </div>
         <div>※ Score multiplier is inaccurate for parameters above 2400.</div>
         <StageSelect />
         {stage.type == "event" ? (
@@ -243,7 +243,7 @@ export default function Simulator() {
             href="https://github.com/surisuririsu/gakumas-tools/blob/master/gakumas-tools/simulator/CHANGELOG.md"
             target="_blank"
           >
-            {t("lastUpdated")}: 2025-06-29
+            {t("lastUpdated")}: 2025-07-03
           </a>
         </div>
         {!simulatorData && (
