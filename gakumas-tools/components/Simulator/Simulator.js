064cb2cadd8131a59936e9a3786f2d3102733d68
risりす <surisuririsu@gmail.com>
2025-07-07 03:55:33 -0400

    Add new p-item and customizations

--- gakumas-tools/components/Simulator/Simulator.js
+++ gakumas-tools/components/Simulator/Simulator.js
@@ -243,7 +243,7 @@ export default function Simulator() {
             href="https://github.com/surisuririsu/gakumas-tools/blob/master/gakumas-tools/simulator/CHANGELOG.md"
             target="_blank"
           >
-            {t("lastUpdated")}: 2025-07-04
+            {t("lastUpdated")}: 2025-07-07
           </a>
         </div>
         {!simulatorData && (
