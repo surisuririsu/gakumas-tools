eee6fa44ef418212e9fd3434bcb0c3b9b622958b
risりす <surisuririsu@gmail.com>
2025-07-31 17:32:15 -0400

    Restyle to accommodate expandable video component

--- gakumas-tools/components/Navbar/Navbar.js
+++ gakumas-tools/components/Navbar/Navbar.js
@@ -1,8 +1,11 @@
 "use client";
 import { memo } from "react";
 import { useTranslations } from "next-intl";
-import ToolHeader from "@/components/ToolHeader";
 import { Link, usePathname } from "@/i18n/routing";
+import Oshi from "@/components/Oshi";
+import ToolHeader from "@/components/ToolHeader";
+import c from "@/utils/classNames";
+import { OSHI_PROPS } from "@/utils/oshi";
 import { TOOLS } from "@/utils/tools";
 import NavbarLink from "./NavbarLink";
 import NavbarMenu from "./NavbarMenu";
@@ -13,26 +16,30 @@ function Navbar() {
   const pathname = usePathname();
 
   return (
-    <nav className={styles.navbar}>
-      <Link href="/">
-        <h1>Gakumas Tools</h1>
-      </Link>
+    <>
+      <nav className={c(styles.navbar, OSHI_PROPS && styles.hasOshi)}>
+        <Link href="/">
+          <h1>Gakumas Tools</h1>
+        </Link>
 
-      <div className={styles.links}>
-        {Object.keys(TOOLS).map((key) => (
-          <NavbarLink
-            key={key}
-            icon={TOOLS[key].icon}
-            path={TOOLS[key].path}
-            title={t(`${key}.title`)}
-            active={pathname.startsWith(TOOLS[key].path)}
-          />
-        ))}
-      </div>
-
-      <NavbarMenu />
-      <ToolHeader />
-    </nav>
+        <div className={styles.links}>
+          {Object.keys(TOOLS).map((key) => (
+            <NavbarLink
+              key={key}
+              icon={TOOLS[key].icon}
+              path={TOOLS[key].path}
+              title={t(`${key}.title`)}
+              active={pathname.startsWith(TOOLS[key].path)}
+            />
+          ))}
+        </div>
+        <div className={styles.right}>
+          <ToolHeader />
+          <NavbarMenu />
+        </div>
+      </nav>
+      {OSHI_PROPS && <Oshi {...OSHI_PROPS} />}
+    </>
   );
 }
 
---
+++ gakumas-tools/utils/oshi.js
@@ -0,0 +1,5 @@
+export const OSHI_PROPS = {
+  text: "倉本千奈 生誕ミニライブ2025",
+  initiallyExpanded: true,
+  videoId: "CD93hMXh6Yc",
+};
