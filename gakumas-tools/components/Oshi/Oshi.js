d612c2fdc381454b15cb579188ba19c848fb149a
risりす <surisuririsu@gmail.com>
2025-08-01 01:16:23 -0400

    Add support for links in oshi component

--- gakumas-tools/components/Oshi/Oshi.js
+++ gakumas-tools/components/Oshi/Oshi.js
@@ -3,37 +3,52 @@ import { useState } from "react";
 import { Resizable } from "re-resizable";
 import styles from "./Oshi.module.scss";
 
-export default function Oshi({ text, initiallyExpanded, videoId }) {
+export default function Oshi({ text, initiallyExpanded, videoId, url }) {
   const [expanded, setExpanded] = useState(initiallyExpanded);
 
-  return (
-    <div className={styles.oshi}>
-      {expanded ? (
-        <>
-          <Resizable
-            className={styles.wrapper}
-            defaultSize={{ width: "100%", height: "300px" }}
-            handleClasses={{ bottom: styles.handle }}
-            enable={{ bottom: true }}
-          >
-            <iframe
-              src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
-              allowFullScreen
-            ></iframe>
-          </Resizable>
-          <button className={styles.close} onClick={() => setExpanded(false)}>
-            ×
-          </button>
-        </>
-      ) : (
-        <button className={styles.expand} onClick={() => setExpanded(true)}>
+  if (url) {
+    return (
+      <div className={styles.oshi}>
+        <a
+          className={styles.expand}
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           {text}
-        </button>
-      )}
-    </div>
-  );
+        </a>
+      </div>
+    );
+  } else if (videoId) {
+    return (
+      <div className={styles.oshi}>
+        {expanded ? (
+          <>
+            <Resizable
+              className={styles.wrapper}
+              defaultSize={{ width: "100%", height: "300px" }}
+              handleClasses={{ bottom: styles.handle }}
+              enable={{ bottom: true }}
+            >
+              <iframe
+                src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
+            </Resizable>
+            <button className={styles.close} onClick={() => setExpanded(false)}>
+              ×
+            </button>
+          </>
+        ) : (
+          <button className={styles.expand} onClick={() => setExpanded(true)}>
+            {text}
+          </button>
+        )}
+      </div>
+    );
+  }
 }
