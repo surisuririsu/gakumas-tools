380ade92596a050fad67cce6e5966a337e8a9d94
risりす <surisuririsu@gmail.com>
2025-08-01 11:01:41 -0400

    Turn off China live embed

--- gakumas-tools/utils/oshi.js
+++ gakumas-tools/utils/oshi.js
@@ -1,6 +1,8 @@
-export const OSHI_PROPS = {
-  text: "倉本千奈 生誕ミニライブ2025",
-  initiallyExpanded: true,
-  videoId: "CD93hMXh6Yc",
-  // url: "https://www.youtube.com/watch?v=CD93hMXh6Yc",
-};
+// {
+//   text: "倉本千奈 生誕ミニライブ2025",
+//   initiallyExpanded: true,
+//   videoId: "CD93hMXh6Yc",
+//   url: "https://www.youtube.com/watch?v=CD93hMXh6Yc",
+// }
+
+export const OSHI_PROPS = null;
