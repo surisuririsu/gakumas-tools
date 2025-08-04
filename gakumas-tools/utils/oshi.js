7604ceae6737e0cebe45aa67f7ccef97ef6f0b11
risりす <surisuririsu@gmail.com>
2025-08-04 14:22:44 -0400

    :)

--- gakumas-tools/utils/oshi.js
+++ gakumas-tools/utils/oshi.js
@@ -5,30 +5,46 @@ import Image from "@/components/Image";
 const IdolIcon = ({ idolId }) => {
   const idol = Idols.getById(idolId);
   return (
-    <Image src={gkImg(idol).icon} width={24} height={24} draggable={false} />
+    <Image
+      style={{ verticalAlign: "sub" }}
+      src={gkImg(idol).icon}
+      alt=""
+      width={24}
+      height={24}
+      draggable={false}
+    />
   );
 };
 
 export const OSHI_PROPS = {
   text: (
-    <>
-      「初星学園放送部 #49」を聴きましょう！
+    <div>
+      今夜20時に、
       <IdolIcon idolId={6} />
-      <IdolIcon idolId={10} />
-    </>
+      伊藤舞音・浅見香月の『おかづラジオ』3回目の配信があります！
+      <br />
+      <IdolIcon idolId={8} />
+      川村玲奈さんもゲストで登場します！
+      <br />
+      一部無料で視聴できるので、ぜひ観てみてください！
+    </div>
   ),
-  url: "https://asobichannel.asobistore.jp/watch/9f01driiwg2",
+  url: "https://nicochannel.jp/okazuradio/live/smSKfik57mLfTYfT2nWvCZ2d",
 };
 
-// {
-//   text: "初星学園放送部 #49",
-//   initiallyExpanded: true,
-//   type: "asobiChannel",
-//   videoId: "f2c1417b",
-// }
+// export const OSHI_PROPS = {
+//   text: (
+//     <>
+//       「初星学園放送部 #49」を聴きましょう！
+//       <IdolIcon idolId={6} />
+//       <IdolIcon idolId={10} />
+//     </>
+//   ),
+//   url: "https://asobichannel.asobistore.jp/watch/9f01driiwg2",
+// };
+
 // export const OSHI_PROPS = {
 //   text: "倉本千奈 生誕ミニライブ2025",
 //   initiallyExpanded: true,
-//   type: "youtube",
 //   videoId: "CD93hMXh6Yc",
 // };
