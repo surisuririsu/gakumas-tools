406b4a9a8c305c451c504ea603b4ada28da0c1d2
risりす <surisuririsu@gmail.com>
2025-09-03 07:51:06 -0400

    Hide

--- gakumas-tools/utils/oshi.js
+++ gakumas-tools/utils/oshi.js
@@ -29,19 +29,21 @@ const IdolIcon = ({ idolId }) => {
   );
 };
 
-export const OSHI_PROPS = {
-  text: (
-    <div>
-      今夜20時に
-      <IdolIcon idolId={6} />
-      伊藤舞音・浅見香月の『おかづラジオ』5回目の配信があります！
-      <IdolIcon idolId={5} />
-      花岩香奈さんもゲストで登場します！ぜひ観てみてください！
-    </div>
-  ),
-  initiallyExpanded: true,
-  url: "https://nicochannel.jp/okazuradio/live/smXBG4dkhNHQJqMb774yQNyr",
-};
+export const OSHI_PROPS = null;
+
+// export const OSHI_PROPS = {
+//   text: (
+//     <div>
+//       今夜20時に
+//       <IdolIcon idolId={6} />
+//       伊藤舞音・浅見香月の『おかづラジオ』5回目の配信があります！
+//       <IdolIcon idolId={5} />
+//       花岩香奈さんもゲストで登場します！ぜひ観てみてください！
+//     </div>
+//   ),
+//   initiallyExpanded: true,
+//   url: "https://nicochannel.jp/okazuradio/live/smXBG4dkhNHQJqMb774yQNyr",
+// };
 
 // export const OSHI_PROPS = {
 //   text: (
