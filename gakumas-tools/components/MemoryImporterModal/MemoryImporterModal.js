f8be620d62d093d6008d33bd04fd241ae85c7a56
risりす <surisuririsu@gmail.com>
2025-09-16 14:30:44 -0400

    Use linear classifier instead of embedding similarity

--- gakumas-tools/components/MemoryImporterModal/MemoryImporterModal.js
+++ gakumas-tools/components/MemoryImporterModal/MemoryImporterModal.js
@@ -52,15 +52,15 @@ function MemoryImporterModal({ onSuccess, multiple = true }) {
     const pItemSession = await ort.InferenceSession.create(
       "/p_item_model.onnx"
     );
-    const pItemEmbeddings = await fetch("/p_item_embeddings.json").then((r) =>
+    const pItemClasses = await fetch("/p_item_classes.json").then((r) =>
       r.json()
     );
 
     const skillCardSession = await ort.InferenceSession.create(
       "/skill_card_model.onnx"
     );
-    const skillCardEmbeddings = await fetch("/skill_card_embeddings.json").then(
-      (r) => r.json()
+    const skillCardClasses = await fetch("/skill_card_classes.json").then((r) =>
+      r.json()
     );
 
     let results = [];
@@ -76,9 +76,9 @@ function MemoryImporterModal({ onSuccess, multiple = true }) {
           file,
           engWorker,
           pItemSession,
-          pItemEmbeddings,
+          pItemClasses,
           skillCardSession,
-          skillCardEmbeddings
+          skillCardClasses
         );
         setProgress((p) => p + 1);
         return memory;
