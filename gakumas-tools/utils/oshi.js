54a97143568f3ca82c1c3257eca6238d4c2134bb
risりす <surisuririsu@gmail.com>
2025-09-21 05:46:56 -0400

    Hide oshi banner

--- gakumas-tools/utils/oshi.js
+++ gakumas-tools/utils/oshi.js
@@ -16,8 +16,4 @@ const IdolIcon = ({ idolId }) => {
   );
 };
 
-export const OSHI_PROPS = {
-  text: "9.20→21「クラス対抗初星大運動会」配信チケット発売中！",
-  initiallyExpanded: true,
-  url: "https://asobistage.asobistore.jp/event/gakuen_gkmas_undokai/ticket",
-};
+export const OSHI_PROPS = null;
