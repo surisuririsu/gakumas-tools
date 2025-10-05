27a0094fe19fd7efec032d5215d841174408b49d
risりす <surisuririsu@gmail.com>
2025-10-04 21:12:10 -0400

    :)

--- gakumas-tools/utils/oshi.js
+++ gakumas-tools/utils/oshi.js
@@ -16,4 +16,16 @@ const IdolIcon = ({ idolId }) => {
   );
 };
 
-export const OSHI_PROPS = null;
+export const OSHI_PROPS = {
+  text: (
+    <div>
+      今日20時に、「三宅麻理恵のゲーマーズギルド」に
+      <IdolIcon idolId={6} />
+      伊藤舞音さんがゲストで出演します！
+      <br />
+      番組前半はどなたでもご視聴いただけます！
+    </div>
+  ),
+  initiallyExpanded: true,
+  url: "https://live.nicovideo.jp/watch/lv348799112",
+};
