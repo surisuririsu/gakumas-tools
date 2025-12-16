c3a39c7705039f9531f16b38d862e580686e94f0
risりす <surisuririsu@gmail.com>
2025-12-16 03:11:10 -0500

    S36 preview

--- gakumas-tools/utils/oshi.js
+++ gakumas-tools/utils/oshi.js
@@ -16,15 +16,4 @@ const IdolIcon = ({ idolId }) => {
   );
 };
 
-export const OSHI_PROPS = {
-  text: (
-    <div>
-      『THE IDOLM@STER M@STERS OF IDOL WORLD 2025』配信チケット販売中
-      <br />
-      <IdolIcon idolId={6} />
-      倉本千奈ちゃんはDAY2に出演します！
-    </div>
-  ),
-  initiallyExpanded: true,
-  url: "https://asobistage.asobistore.jp/event/idolmaster_idolworld2025/ticket",
-};
+export const OSHI_PROPS = null;
