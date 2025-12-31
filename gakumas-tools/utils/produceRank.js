3d8891e215f8d11af08bf830fbb16bca29931c26
risりす <surisuririsu@gmail.com>
2025-12-30 21:54:53 -0500

    Add Hajime Legend calculation

--- gakumas-tools/utils/produceRank.js
+++ gakumas-tools/utils/produceRank.js
@@ -2,12 +2,20 @@ export const MAX_PARAMS_BY_DIFFICULTY = {
   regular: 1000,
   pro: 1500,
   master: 1800,
+  legend: 2800,
 };
 
 export const PARAM_BONUS_BY_PLACE = {
   1: 30,
   2: 20,
   3: 10,
+  4: 0,
+};
+
+export const PARAM_BONUS_BY_PLACE_LEGEND = {
+  1: 120,
+  2: 100,
+  3: 80,
   4: 0,
 };
 
@@ -19,6 +27,7 @@ export const RATING_BY_PLACE = {
 };
 
 export const TARGET_RATING_BY_RANK = {
+  S4: 26000,
   "SSS+": 23000,
   SSS: 20000,
   "SS+": 18000,
@@ -42,22 +51,73 @@ export const REVERSE_RATING_REGIMES = [
   { threshold: 0, base: 0, multiplier: 0.3 },
 ];
 
-export function calculateRatingExExamScore(place, params, maxParams) {
-  const placeParamBonus = PARAM_BONUS_BY_PLACE[place];
+export const REVERSE_RATING_REGIMES_LEGEND_MIDTERM = [
+  { base: 200000, multiplier: 0 },
+  { base: 60000, multiplier: 0.001 },
+  { base: 50000, multiplier: 0.002 },
+  { base: 40000, multiplier: 0.003 },
+  { base: 30000, multiplier: 0.008 },
+  { base: 20000, multiplier: 0.05 },
+  { base: 10000, multiplier: 0.08 },
+  { base: 0, multiplier: 0.11 },
+];
+
+export const REVERSE_RATING_REGIMES_LEGEND_FINAL = [
+  { threshold: 8700, base: 2000000, multiplier: 0 },
+  { threshold: 7300, base: 600000, multiplier: 0.001 },
+  { threshold: 6500, base: 500000, multiplier: 0.008 },
+  { threshold: 4500, base: 300000, multiplier: 0.01 },
+  { threshold: 0, base: 0, multiplier: 0.015 },
+];
+
+const PARAM_RATING_MULTIPLIER = 2.3;
+
+const PARAM_RATING_MULTIPLIER_LEGEND = 2.1;
+
+export function calculateRatingExExamScore(
+  place,
+  params,
+  maxParams,
+  midtermScore,
+  difficulty
+) {
+  const PARAM_RATING_MULTIPLIER_USED =
+    difficulty === "legend"
+      ? PARAM_RATING_MULTIPLIER_LEGEND
+      : PARAM_RATING_MULTIPLIER;
+  const PARAM_BONUS_BY_PLACE_USED =
+    difficulty === "legend"
+      ? PARAM_BONUS_BY_PLACE_LEGEND
+      : PARAM_BONUS_BY_PLACE;
+  const placeParamBonus = PARAM_BONUS_BY_PLACE_USED[place];
   const placeRating = RATING_BY_PLACE[place];
   const paramRating = Math.floor(
     params.reduce(
       (acc, cur) => acc + Math.min(cur + placeParamBonus, maxParams),
       0
-    ) * 2.3
+    ) * PARAM_RATING_MULTIPLIER_USED
   );
-  return placeRating + paramRating;
+  let midtermRating = 0;
+  if (difficulty == "legend") {
+    for (let { base, multiplier } of REVERSE_RATING_REGIMES_LEGEND_MIDTERM) {
+      if (midtermScore > base) {
+        midtermRating += (midtermScore - base) * multiplier;
+        midtermScore = base;
+      }
+    }
+    midtermRating = Math.floor(midtermRating);
+  }
+  return placeRating + paramRating + midtermRating;
 }
 
-export function calculateTargetScores(ratingExExamScore) {
+export function calculateTargetScores(ratingExExamScore, difficulty) {
+  const REVERSE_RATING_REGIMES_USED =
+    difficulty === "legend"
+      ? REVERSE_RATING_REGIMES_LEGEND_FINAL
+      : REVERSE_RATING_REGIMES;
   return Object.keys(TARGET_RATING_BY_RANK).map((rank) => {
     const targetRating = TARGET_RATING_BY_RANK[rank] - ratingExExamScore;
-    for (let { threshold, base, multiplier } of REVERSE_RATING_REGIMES) {
+    for (let { threshold, base, multiplier } of REVERSE_RATING_REGIMES_USED) {
       if (targetRating <= threshold) continue;
       return {
         rank,
@@ -68,10 +128,18 @@ export function calculateTargetScores(ratingExExamScore) {
   });
 }
 
-export function calculateActualRating(actualScore, ratingExExamScore) {
+export function calculateActualRating(
+  actualScore,
+  ratingExExamScore,
+  difficulty
+) {
+  const REVERSE_RATING_REGIMES_USED =
+    difficulty === "legend"
+      ? REVERSE_RATING_REGIMES_LEGEND_FINAL
+      : REVERSE_RATING_REGIMES;
   let calcScore = actualScore;
   let actualRating = 0;
-  for (let { base, multiplier } of REVERSE_RATING_REGIMES) {
+  for (let { base, multiplier } of REVERSE_RATING_REGIMES_USED) {
     if (calcScore > base) {
       actualRating += (calcScore - base) * multiplier;
       calcScore = base;
