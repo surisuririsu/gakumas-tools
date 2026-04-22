fe8e6c748939b2a2f34b515ddbebceb1a0806503
risりす <surisuririsu@gmail.com>
2026-04-22 15:06:24 -0400

    Make modal scrollable

--- gakumas-tools/components/SimulationRuns/LoadoutsModal.js
+++ gakumas-tools/components/SimulationRuns/LoadoutsModal.js
@@ -3,12 +3,15 @@ import { useContext } from "react";
 import Modal from "@/components/Modal";
 import ModalContext from "@/contexts/ModalContext";
 import CompareTab from "./CompareTab";
+import styles from "./SimulationRuns.module.scss";
 
 export default function LoadoutsModal() {
   const { closeModal } = useContext(ModalContext);
   return (
     <Modal>
-      <CompareTab currentRun={null} onAfterLoad={closeModal} />
+      <div className={styles.modalScroll}>
+        <CompareTab currentRun={null} onAfterLoad={closeModal} />
+      </div>
     </Modal>
   );
 }
