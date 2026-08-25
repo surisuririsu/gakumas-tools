872278f16ae3d9cdc052860f68d3b6b1905bf578
unoeins <twintailsister@gmail.com>
2026-08-25 15:59:30 +0900

    Add nee icchauyo misuzu preview
    (cherry picked from commit 1721f1a3d01b883dbb64d88b4d43f23e0c243422)

--- packages/gakumas-engine/engine/CardManager/index.js
+++ packages/gakumas-engine/engine/CardManager/index.js
@@ -492,6 +492,7 @@ export default class CardManager extends EngineComponent {
       state[S.activeCardsUsed]++;
     }
 
+    const beforeCardUsedState = conditionState;
     conditionState = shallowCopy(state);
 
     if (state[S.thisCardHeld]) {
@@ -518,6 +519,7 @@ export default class CardManager extends EngineComponent {
       this.engine.effectManager.triggerEffectsForPhase(
         state,
         "buffCostConsumed",
+        beforeCardUsedState,
       );
       state[S.buffCostConsumed] = false;
     }
